d2c5d1d5bf22513a6f28a1cbc19f0f5d2747df51
Mann Yivfouy <mannyivfouy@gmail.com>
2026-07-03 15:02:12 +0700

    update pagination design and func

--- src/app/shared/components/pagination/pagination.ts
+++ src/app/shared/components/pagination/pagination.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LucideAngularModule, ChevronLeft, ChevronRight } from 'lucide-angular';
 
 @Component({
   selector: 'app-pagination',
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './pagination.html',
   styleUrl: './pagination.css',
 })
@@ -11,13 +12,31 @@ export class Pagination {
   @Input() page = 1;
   @Input() totalPage = 1;
 
+  @Input() limit = 10;
+  @Input() totalItems = 0;
+
   @Output() pageChange = new EventEmitter<number>();
 
+  icons = {
+    ChevronLeft,
+    ChevronRight,
+  };
+
   changePage(p: number) {
+    if (p < 1 || p > this.totalPage) return;
     this.pageChange.emit(p);
   }
 
   get pages(): number[] {
     return Array.from({ length: this.totalPage }, (_, i) => i + 1);
+  }
+
+  get startItem(): number {
+    if (this.totalItems === 0) return 0;
+    return (this.page - 1) * this.limit + 1;
+  }
+
+  get endItem(): number {
+    return Math.min(this.page * this.limit, this.totalItems);
   }
 }
