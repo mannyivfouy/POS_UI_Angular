4b6e6e4479ddd151085d452dcf84db46cf18bb3f
Mann Yivfouy <mannyivfouy@gmail.com>
2026-07-05 23:01:07 +0700

    implement product, category and supplier models

---
+++ src/app/core/services/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private apiUrl = `${environment.apiUrl}/products`;
+
+  constructor(private http: HttpClient){}
+
+  
+}
