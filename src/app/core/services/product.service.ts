6e6b7635f30e32c6a3e35c4b6dc5866b5aa677df
Mann Yivfouy <mannyivfouy@gmail.com>
2026-07-06 16:37:19 +0700

    get all product in product-service

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PaginatedResponse } from '../models/paginated-response.model';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +11,33 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class ProductService {
   private apiUrl = `${environment.apiUrl}/products`;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  
+  getProducts(params: {
+    page?: number;
+    limit?: number;
+    search?: string;
+  }): Observable<PaginatedResponse<Product>> {
+    let httpParams = new HttpParams();
+
+    if (params.page != null) {
+      httpParams = httpParams.set('page', String(params.page));
+    }
+
+    if (params.page != null) {
+      httpParams = httpParams.set('limit', String(params.page));
+    }
+
+    if (params.page != null) {
+      httpParams = httpParams.set('search', String(params.page));
+    }
+
+    return this.http.get<PaginatedResponse<Product>>(this.apiUrl, {
+      params: httpParams,
+    });
+  }
+
+  // getProductStats(): Observable<> {
+  //     return this.http.get<UserStats>(`${this.apiUrl}/stats`);
+  //   }
 }
