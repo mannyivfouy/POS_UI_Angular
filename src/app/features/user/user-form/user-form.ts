80fb1908ae19f18f338112e6b7608eb89d7462fe
Mann Yivfouy <mannyivfouy@gmail.com>
2026-07-16 18:17:12 +0700

    fix drawer UI design

--- src/app/features/user/user-form/user-form.ts
+++ src/app/features/user/user-form/user-form.ts
@@ -1,18 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { LucideAngularModule, Save, X } from 'lucide-angular';
 @Component({
   selector: 'app-user-form',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, LucideAngularModule],
   templateUrl: './user-form.html',
   styleUrl: './user-form.css',
 })
 export class UserForm {
   @Input() user: any = null;
-
   @Output() saved = new EventEmitter<any>();
   @Output() cancel = new EventEmitter<void>();
+
+  icons = {
+    X,
+    Save
+  }
 
   userData = {
     fullname: '',
--- src/app/shared/components/drawer/drawer.ts
+++ src/app/shared/components/drawer/drawer.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LucideAngularModule, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-drawer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './drawer.html',
   styleUrl: './drawer.css',
 })
@@ -11,6 +12,10 @@ export class Drawer {
   @Input() title = '';
 
   @Output() close = new EventEmitter<void>();
+
+  icons = {
+    X,
+  }
 
   onClose() {
     this.close.emit();
