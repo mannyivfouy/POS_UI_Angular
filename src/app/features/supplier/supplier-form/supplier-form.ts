6d2a9e66af9083c2bc60375b814b64ff057b51c3
Mann Yivfouy <mannyivfouy@gmail.com>
2026-07-20 00:10:15 +0700

    crud supplier and fix the list and form ui design

--- src/app/features/supplier/supplier-form/supplier-form.ts
+++ src/app/features/supplier/supplier-form/supplier-form.ts
@@ -1,11 +1,109 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LucideAngularModule, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-supplier-form',
-  imports: [],
+  imports: [LucideAngularModule, CommonModule, FormsModule, ReactiveFormsModule, TranslatePipe],
   templateUrl: './supplier-form.html',
   styleUrl: './supplier-form.css',
 })
-export class SupplierForm {
+export class SupplierForm implements OnChanges {
+  @Input() supplierData: any = null;
 
+  @Output() saved = new EventEmitter<any>();
+  @Output() cancel = new EventEmitter<void>();
+
+  icons = {
+    X,
+  };
+
+  supplierForm!: FormGroup;
+  isEditingMode = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.supplierForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      contactPerson: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      phone: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
+      address: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      status: ['active', Validators.required],
+      note: ['', [Validators.maxLength(100)]],
+    });
+
+    this.loadSupplier();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['supplierData'] && this.supplierForm) {
+      this.loadSupplier();
+    }
+  }
+
+  loadSupplier() {
+    if (!this.supplierData) {
+      this.isEditingMode = false;
+      this.supplierForm.reset({
+        status: 'active',
+      });
+      return;
+    }
+
+    this.isEditingMode = true;
+    this.supplierForm.patchValue({
+      name: this.supplierData.name,
+      contactPerson: this.supplierData.contactPerson,
+      phone: this.supplierData.phone,
+      email: this.supplierData.email,
+      address: this.supplierData.address,
+      status: this.supplierData.status,
+      note: this.supplierData.note,
+    });
+  }
+
+  toggleStatus() {
+    const currentStatus = this.supplierForm.get('status')?.value;
+
+    this.supplierForm.patchValue({
+      status: currentStatus === 'active' ? 'inactive' : 'active',
+    });
+  }
+
+  submit() {
+    if (this.supplierForm.invalid) {
+      this.supplierForm.markAllAsTouched();
+      return;
+    }
+
+    this.saved.emit(this.supplierForm.value);
+  }
+
+  onCancel() {
+    this.cancel.emit();
+  }
+
+  resetForm() {
+    this.isEditingMode = false;
+
+    this.supplierForm.reset({
+      name: '',
+      contactPerson: '',
+      phone: '',
+      email: '',
+      address: '',
+      status: 'active',
+      note: '',
+    });
+  }
 }
--- src/app/features/supplier/supplier-list/supplier-list.ts
+++ src/app/features/supplier/supplier-list/supplier-list.ts
@@ -1,120 +1,228 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { Contact, LucideAngularModule, Pencil, Plus, Trash2 } from 'lucide-angular';
-import { Supplier } from '../../../core/models/supplier.model';
-import { StatsCardModel } from '../../../shared/models/stats-card.model';
-import { SupplierService } from '../../../core/services/supplier.service';
-import { LoadingScreenService } from '../../../core/services/loading.service';
-import { TranslatePipe } from '@ngx-translate/core';
-import { StatsGrid } from '../../../shared/components/stats-grid/stats-grid';
-import { Search } from '../../../shared/components/search/search';
-import { CommonModule } from '@angular/common';
+  import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+  import { ChevronDown, Contact, LucideAngularModule, Pencil, Plus, Trash2 } from 'lucide-angular';
+  import { Supplier } from '../../../core/models/supplier.model';
+  import { StatsCardModel } from '../../../shared/models/stats-card.model';
+  import { SupplierService } from '../../../core/services/supplier.service';
+  import { TranslatePipe } from '@ngx-translate/core';
+  import { StatsGrid } from '../../../shared/components/stats-grid/stats-grid';
+  import { Search } from '../../../shared/components/search/search';
+  import { CommonModule } from '@angular/common';
+  import { Drawer } from '../../../shared/components/drawer/drawer';
+  import { SupplierForm } from '../supplier-form/supplier-form';
+  import { ActivatedRoute, Router } from '@angular/router';
+  import { Pagination } from "../../../shared/components/pagination/pagination";
+import { ConfirmDialog } from "../../../shared/components/confirm-dialog/confirm-dialog";
 
-@Component({
-  selector: 'app-supplier-list',
-  imports: [LucideAngularModule, TranslatePipe, StatsGrid, Search, CommonModule],
-  templateUrl: './supplier-list.html',
-  styleUrl: './supplier-list.css',
-})
-export class SupplierList {
-  icons = {
-    Plus,
-    Contact,
-    Pencil,
-    Trash2,
-  };
+  @Component({
+    selector: 'app-supplier-list',
+    imports: [LucideAngularModule, TranslatePipe, StatsGrid, Search, CommonModule, Pagination, Drawer, SupplierForm, ConfirmDialog],
+    templateUrl: './supplier-list.html',
+    styleUrl: './supplier-list.css',
+  })
+  export class SupplierList {
+    @ViewChild(Drawer)
+    drawer?: Drawer;
 
-  suppliers: Supplier[] = [];
-  stats: StatsCardModel[] = [];
+    @ViewChild(SupplierForm)
+    supplierForm?: SupplierForm;
 
-  page = 1;
-  limit = 10;
-  totalPage = 1;
-  totalItems = 0;
-  searchKeyword = '';
+    icons = {
+      Plus,
+      Contact,
+      Pencil,
+      Trash2,
+      ChevronDown
+    };
 
-  loading = false;
+    suppliers: Supplier[] = [];
+    stats: StatsCardModel[] = [];
 
-  isDrawerOpen = false;
-  selectedSupplier: Supplier | null = null;
+    page = 1;
+    limit = 10;
+    totalPage = 1;
+    totalItems = 0;
 
-  constructor(
-    private supplierService: SupplierService,
-    private cdr: ChangeDetectorRef,
-    private loadingScreenService: LoadingScreenService,
-  ) {}
+    searchKeyword = '';
+    statusFilter = '';
 
-  ngOnInit(): void {
-    this.loadSuppliers();
-    this.loadStats();
-  }
+    loading = false;
 
-  changePage(page: number): void {
-    this.page = page;
-    this.loadSuppliers();
-  }
+    isDrawerOpen = false;
+    statusDropdownOpen = false;
+    showDeleteDialog = false;
 
-  onSearch(keyboard: string) {
-    this.searchKeyword = keyboard;
-    this.page = 1;
-    this.loadSuppliers();
-  }
+    selectedSupplier: Supplier | null = null;
 
-  loadSuppliers(): void {
-    this.loading = true;
-    this.supplierService
-      .getSuppliers({
-        page: this.page,
-        limit: this.limit,
-        search: this.searchKeyword,
-      })
-      .subscribe({
+    constructor(
+      private supplierService: SupplierService,
+      private cdr: ChangeDetectorRef,
+      private router: Router,
+      private route: ActivatedRoute,
+    ) {}
+
+    ngOnInit(): void {
+      this.route.queryParams.subscribe((param) => {
+        ((this.page = +(param['page'] ?? 1)),
+          (this.limit = +(param['limit'] ?? 10)),
+          (this.searchKeyword = param['search'] ?? ''),
+          (this.statusFilter = param['status'] ?? ''),
+          this.loadSuppliers());
+      });
+      this.loadStats();
+    }
+
+    changePage(page: number): void {
+      this.page = page;
+      this.updateQueryParams();
+    }
+
+    onSearch(keyboard: string) {
+      this.searchKeyword = keyboard;
+      this.page = 1;
+      this.updateQueryParams();
+    }
+
+    toggleStatusDropdown() {
+      this.statusDropdownOpen = !this.statusDropdownOpen;
+    }
+
+    onStatusFilter(status: string) {
+      this.statusFilter = status;
+      this.statusDropdownOpen = false;
+      this.page = 1;
+      this.updateQueryParams();
+    }
+
+    private updateQueryParams(): void {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: {
+          page: this.page,
+          limit: this.limit,
+          search: this.searchKeyword || null,
+          status: this.statusFilter || null,
+        },
+        queryParamsHandling: 'merge',
+      });
+    }
+
+    loadSuppliers(): void {
+      this.loading = true;
+      this.supplierService
+        .getSuppliers({
+          page: this.page,
+          limit: this.limit,
+          search: this.searchKeyword,
+          status: this.statusFilter,
+        })
+        .subscribe({
+          next: (res) => {
+            this.suppliers = res.data;
+
+            this.limit = res.pagination.limit;
+            this.totalPage = res.pagination.totalPage;
+            this.totalItems = res.pagination.total;
+
+            this.loading = false;
+            this.cdr.detectChanges();
+          },
+          error: () => {
+            this.loading = false;
+          },
+        });
+    }
+
+    loadStats(): void {
+      this.supplierService.getSupplierStats().subscribe({
         next: (res) => {
-          console.log(res);
-          this.suppliers = res.data;
-
-          this.limit = res.pagination.limit;
-          this.totalPage = res.pagination.totalPage;
-          this.totalItems = res.pagination.total;
-
-          this.loading = true;
+          const stats = res.data;
+          this.stats = [
+            { titleKey: 'supplier.stats.total', value: stats.totalSupplier, icon: Contact },
+            // { titleKey: 'supplier.stats.active', value: stats.activeProduct, icon: ArchiveRestore },
+            // { titleKey: 'supplier.stats.inactive', value: stats.inactiveProduct, icon: ArchiveX },
+          ];
           this.cdr.detectChanges();
         },
         error: () => {
           this.loading = false;
         },
       });
-  }
+    }
 
-  loadStats(): void {
-    this.supplierService.getSupplierStats().subscribe({
-      next: (res) => {
-        const stats = res.data;
-        this.stats = [
-          { titleKey: 'supplier.stats.total', value: stats.totalSupplier, icon: Contact },
-          // { titleKey: 'supplier.stats.active', value: stats.activeProduct, icon: ArchiveRestore },
-          // { titleKey: 'supplier.stats.inactive', value: stats.inactiveProduct, icon: ArchiveX },
-        ];
-        this.cdr.detectChanges();
-      },
-      error: () => {
-        this.loading = false;
-      },
-    });
-  }
+    openCreateSupplier() {
+      this.selectedSupplier = null;
+      this.isDrawerOpen = true;
+    }
 
-  openCreateSupplier() {
-    this.selectedSupplier = null;
-    this.isDrawerOpen = true;
-  }
+    closeDrawer() {
+      this.supplierForm?.resetForm();
 
-  closeDrawer() {
-    this.isDrawerOpen = false;
-  }
+      this.isDrawerOpen = false;
 
-  edit(supplier: Supplier) {
-    this.selectedSupplier = supplier;
-    this.isDrawerOpen = true;
-  }
+      setTimeout(() => {
+        this.selectedSupplier = null;
+      }, 500);
+    }
 
-  delete(supplier: any) {}
-}
+    onCancel() {
+      this.drawer?.onClose();
+    }
+
+    onSave(formData: FormData) {
+      if (this.selectedSupplier) {
+        this.supplierService.updateSupplier(this.selectedSupplier._id, formData).subscribe({
+          next: (res) => {
+            this.loadSuppliers();
+            this.onCancel();
+          },
+          error: (err) => {
+            console.error(err);
+          },
+        });
+      } else {
+        this.supplierService.createSupplier(formData).subscribe({
+          next: (res) => {
+            this.loadSuppliers();
+            this.onCancel();
+          },
+          error: (err) => {
+            // console.error(err);
+            console.log('Status:', err.status);
+  console.log('Error:', err.error);
+  console.log('Full error:', err);
+          },
+        });
+      }
+    }
+
+    edit(supplier: Supplier) {
+      this.selectedSupplier = supplier;
+      this.isDrawerOpen = true;
+    }
+
+    delete(supplier: Supplier) {
+      this.selectedSupplier = supplier;
+      this.showDeleteDialog = true;
+    }
+
+    cancelDelete() {
+      this.showDeleteDialog = false;
+      this.selectedSupplier = null;
+    }
+
+    confirmDelete(){
+      this.showDeleteDialog = false;
+      if (!this.selectedSupplier) return;
+
+      this.supplierService.deleteSupplier(this.selectedSupplier._id).subscribe({
+        next: () => {
+          this.selectedSupplier = null;
+          this.loadSuppliers();
+          this.loadStats()
+        },
+        error: (err) => {
+          console.error(err)
+        }
+      })
+    }
+  }
