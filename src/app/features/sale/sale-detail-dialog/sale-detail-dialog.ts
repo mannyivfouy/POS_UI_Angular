711a9bd204a9b8d45468bcf5f86f5030cf22b38f
mannyivfouy <mannyivfouy@gmail.com>
2026-08-05 00:09:37 +0700

    add feature sale-detil

--- src/app/features/sale/sale-detail-dialog/sale-detail-dialog.ts
+++ src/app/features/sale/sale-detail-dialog/sale-detail-dialog.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Sale, SaleItem } from '../../../core/models/sale.model';
+import { LucideAngularModule, Printer, X } from 'lucide-angular';
+import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sale-detail-dialog',
-  imports: [],
+  imports: [LucideAngularModule, CommonModule, TranslatePipe],
   templateUrl: './sale-detail-dialog.html',
   styleUrl: './sale-detail-dialog.css',
 })
 export class SaleDetailDialog {
+  @Input() open = false;
+  @Input() sale: Sale | null = null;
+  @Input() items: SaleItem[] = [];
 
+  @Output() close = new EventEmitter<void>()
+
+  icons = {
+    X,
+    Printer
+  }
+
+  closing = false;
+
+  closingDialog() {
+    this.closing = true;
+  }
+
+  animationEnd() {
+    if (this.closing) {
+      this.closing = false;
+      this.open = false;
+      this.close.emit();
+    }
+  }
 }
